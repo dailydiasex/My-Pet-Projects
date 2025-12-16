93df8aec2495d92c9aa1b99920aa0a553e2afebd
Vlad <newaccforcs2@gmail.com>
2025-12-16 09:04:50 +0200

    Add files via upload

---
+++ modal-project/src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import './index.scss';
+import Modal from './components/Modal';
+
+function App() {
+  const [open, setOpen] = useState(false);
+
+
+  return (
+    <div className="App">
+      <button onClick={() => setOpen(true)} className="open-modal-btn">✨ Открыть окно</button>
+      <Modal open={open} setOpen={setOpen}/>
+    </div>
+  );
+}
+
+export default App;
